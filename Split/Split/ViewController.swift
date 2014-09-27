42b80c030f83d28bba3d36ca6ed3f10f3deae6ca
harry5z <zeyuzeng@andrew.cmu.edu>
2014-09-27 01:08:51 -0400

    simplest user login

--- Split/Split/ViewController.swift
+++ Split/Split/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var errorMessage: UILabel!
+    @IBOutlet var usernameField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    @IBOutlet var emailField: UITextField!
+    
+    @IBAction func onUsernameModified(sender: AnyObject) {
+        var query = PFQuery(className:"User")
+    }
+    
+    @IBAction func onRegister(sender: UIButton) {
+        var newUser = PFUser();
+        newUser.username = self.usernameField.text;
+        newUser.password = self.passwordField.text;
+        newUser.email = self.emailField.text;
+        newUser.signUpInBackgroundWithBlock {
+            (succeeded: Bool!, error: NSError!) -> Void in
+            if error == nil {
+                // Hooray! Let them use the app now.
+            } else {
+                self.errorMessage.text = "Error";
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
