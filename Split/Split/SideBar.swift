bde2e600ce1ab36bc5be98d1f9d8b11088c5fd15
Xiaotian(Adam) Cao <xiaotcao@gmail.com>
2014-09-27 01:09:20 -0400

    sidebar

---
+++ Split/Split/SideBar.swift
@@ -0,0 +1,13 @@
+//
+//  SideBar.swift
+//  Split
+//
+//  Created by Xiaotian Cao on 9/27/14.
+//  Copyright (c) 2014 Xiaotian Cao. All rights reserved.
+//
+
+import UIKit
+
+class SideBar: NSObject, SideBarTableViewControllerDelegate {
+   
+}
--- Split/Split/SideBarTableViewController.swift
+++ Split/Split/SideBarTableViewController.swift
@@ -8,46 +8,70 @@
 
 import UIKit
 
+protocol SideBarTableViewControllerDelegate{
+    func SideBarControlDidSelectRow(indexPath:NSIndexPath) //clicking a cell gives us index path
+}
 class SideBarTableViewController: UITableViewController {
+    
+    var delegate:SideBarTableViewControllerDelegate?
+    var tableData:Array<Event> = []
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Uncomment the following line to preserve selection between presentations
+//        // self.clearsSelectionOnViewWillAppear = false
+//
+//        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+//        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        return tableData.count
     }
 
-    /*
+    
     override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
-
-        // Configure the cell...
+        var cell:UITableViewCell? = tableView.dequeueReusableCellWithIdentifier("Cell") as? UITableViewCell
+        
+        if cell == nil{
+            cell = UITableViewCell (style: UITableViewCellStyle.Default ,reuseIdentifier: "Cell")
+            cell!.backgroundColor = UIColor.clearColor()
+            cell!.textLabel!.textColor= UIColor.darkTextColor()
+            let selectedView:UIView = UIView (frame: CGRect(x: 0, y: 0, width: cell!.frame.size.width, height: cell!.frame.size.height))
+            selectedView.backgroundColor=UIColor.blackColor().colorWithAlphaComponent(0.3)
+            cell!.selectedBackgroundView = selectedView
+        }
+        cell!.textLabel.text= tableData(indexPath.row)
+        
+      //   Configure the cell...
 
         return cell
     }
-    */
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 45.0
+    }
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        delegate?.SideBarControlDidSelectRow(indexPath.row)
+    }
+    
+
 
     /*
     // Override to support conditional editing of the table view.
