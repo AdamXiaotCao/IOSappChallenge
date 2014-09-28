17cd1fbf751e30c627e8076b424fb33edee15b40
Xiaotian(Adam) Cao <xiaotcao@gmail.com>
2014-09-27 23:56:44 -0400

    friend list

---
+++ Split/Split/FriendTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  FriendTableViewCell.swift
+//  Split
+//
+//  Created by Xiaotian Cao on 9/27/14.
+//  Copyright (c) 2014 Xiaotian Cao. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet var firstNameLabel: UILabel!
+    
+    @IBOutlet var lastNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
